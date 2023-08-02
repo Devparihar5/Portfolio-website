62f1ee96d60d4bb96aa14eda1daff219e264a708
Devendra Parihar <devendraparihar340@gmail.com>
2023-08-02 22:45:14 +0100

    portfolio update

--- home.py
+++ home.py
@@ -6,8 +6,9 @@ def home():
     
     # Page configs (tab title, favicon)
     st.set_page_config(
-        page_title="Enric Domingo's Portfolio",
-        page_icon="🍕",
+        page_title="Devendra Parihars's Portfolio",
+        #data symbol
+        page_icon="📊",
     )
 
     # CSS styles file
@@ -15,52 +16,51 @@ def home():
         st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
     # Profile image file
-    with open("assets/profile_squared.png", "rb") as img_file:
+    with open("assets/dev.jpeg", "rb") as img_file:
         img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
 
     # PDF CV file
-    with open("assets/Enric_linkedin_CV.pdf", "rb") as pdf_file:
+    with open("assets/Resume-Devendra_Parihar.pdf", "rb") as pdf_file:
         pdf_bytes = pdf_file.read()
 
     
     # Top title
-    st.write(f"""<div class="title"><strong>Hi! My name is</strong> Enric Domingo👋</div>""", unsafe_allow_html=True)
+    st.write(f"""<div class="title"><strong>Hi! My name is</strong> Devendra Parihar👋</div>""", unsafe_allow_html=True)
 
-    # Profile image
-    st.write(f"""
-    <div class="container">
-        <div class="box">
-            <div class="spin-container">
-                <div class="shape">
-                    <div class="bd">
-                        <img src="{img}" alt="Enric Domingo">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    """, 
-    unsafe_allow_html=True)
-
-    # Alternative image (static and rounded) uncomment it if you prefer this one
+    # # Profile image
     # st.write(f"""
-    # <div style="display: flex; justify-content: center;">
-    #    <img src="{img}" alt="Enric Domingo" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
+    # <div class="container">
+    #     <div class="box">
+    #         <div class="spin-container">
+    #             <div class="shape">
+    #                 <div class="bd">
+    #                     <img src="{img}" alt="Devendra Parihar">
+    #                 </div>
+    #             </div>
+    #         </div>
+    #     </div>
     # </div>
-    # """, unsafe_allow_html=True)
+    # """, 
+    # unsafe_allow_html=True)
+
+    # Alternative image (static and rounded) 
+    st.write(f"""
+    <div style="display: flex; justify-content: center;">
+       <img src="{img}" alt="Enric Domingo" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
+    </div>
+    """, unsafe_allow_html=True)
 
     # Subtitle
-    st.write(f"""<div class="subtitle" style="text-align: center;">Machine Learning and Software Engineer</div>""", unsafe_allow_html=True)
+    st.write(f"""<div class="subtitle" style="text-align: center;">Data Scientist and Machine Learning </div>""", unsafe_allow_html=True)
 
     # Social Icons
     social_icons_data = {
         # Platform: [URL, Icon]
-        "Kaggle": ["https://www.kaggle.com/edomingo", "https://www.kaggle.com/static/images/site-logo.svg"],
-        "LinkedIn": ["https://www.linkedin.com/in/e-domingo", "https://cdn-icons-png.flaticon.com/512/174/174857.png"],
-        "GitHub": ["https://github.com/enricd", "https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg"],
-        "Twitter": ["https://twitter.com/mad_enrico", "https://cdn-icons-png.flaticon.com/512/733/733579.png"],
-        "YouTube": ["https://www.youtube.com/@enricd", "https://cdn-icons-png.flaticon.com/512/1384/1384060.png"],
-        "Medium": ["https://medium.com/@enricdomingo", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Medium_logo_Monogram.svg/1200px-Medium_logo_Monogram.svg.png"]
+        "Kaggle": ["https://www.kaggle.com/dev523", "https://www.kaggle.com/static/images/site-logo.svg"],
+        "LinkedIn": ["https://www.linkedin.com/in/devendra-parihar", "https://cdn-icons-png.flaticon.com/512/174/174857.png"],
+        "GitHub": ["https://github.com/devparihar5", "https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg"],
+        "Twitter": ["https://twitter.com/Devendr74154261", "https://cdn-icons-png.flaticon.com/512/733/733579.png"]
+        # "Medium": ["https://medium.com/@enricdomingo", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Medium_logo_Monogram.svg/1200px-Medium_logo_Monogram.svg.png"]
     }
 
     social_icons_html = [f"<a href='{social_icons_data[platform][0]}' target='_blank' style='margin-right: 10px;'><img class='social-icon' src='{social_icons_data[platform][1]}' alt='{platform}'></a>" for platform in social_icons_data]
@@ -76,19 +76,19 @@ def home():
     # About me section
     st.subheader("About Me")
     st.write("""
-    - 🧑‍💻 I am a **Senior ML and Software Engineer** @ [ERNI](https://www.betterask.erni/) working on a [Boehringer Ingelheim](https://www.boehringer-ingelheim.com)'s pharma project 
+    - 🧑‍💻 **Data Scientist** @ [Kainskep](https://www.kainskep.com/) 
+             
+    - 🎓 **B.Tech** in Artificial Intelligence and Data Science from [Govt. Engineering College Bikaner](https://www.ecb.ac.in/) (2020-2024)
 
-    - 🛩️ prev: Co-founder, Flight Ops Manager and UAS Developer Pilot @ [Venturi Unmanned Technologies](https://www.youtube.com/@venturiunmannedtechnologie2518/featured)
+    - 🛩️ prev: AI/ML Developer Intern in 2-3 startups
 
-    - ❤️ I am passionate about **Machine Learning/Deep Learning, MLOps, Data, Software Engineering, Computer Vision, Bioinformatics, UAVs, Optimization, Automation**, and more!
+    - ❤️ I am passionate about **Data Science, Machine Learning/Deep Learning, Computer Vision, Optimization**, and more!
     
-    - 🤖 I enojoy developing projects such as [SpeedClimbing.AI](https://www.instagram.com/speedclimbing.ai) (🏗️under construction) and participating at platforms like [Kaggle](https://www.kaggle.com/edomingo) 📈
+    - 🤖 I enojoy developing projects such as [Check on github](https://github.com/devparihar5) and participating at platforms like [Kaggle](https://www.kaggle.com/dev523) 📈
+
+    - 📫 How to reach me: devendraparihar340@kainskep.com
     
-    - 🏂 Also practicing sports such as snowboard, wakeboard and climbing 🧗
-    
-    - 📫 How to reach me: contact.enricd@gmail.com
-    
-    - 🏠 Barcelona
+    - 🏠 Jodhpur, Rajasthan, India
     """)
 
     st.write("##")
@@ -97,15 +97,11 @@ def home():
     st.download_button(
         label="📄 Download my CV",
         data=pdf_bytes,
-        file_name="Enric_linkedin_cv.pdf",
+        file_name="Devndra's_cv.pdf",
         mime="application/pdf",
     )
 
     st.write("##")
-    
-    st.write(f"""<div class="subtitle" style="text-align: center;">⬅️ Check out my Projects in the navigation menu! (Coming soon...)</div>""", unsafe_allow_html=True)
-
-
 
 
 if __name__ == "__main__":
